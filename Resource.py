013f9f98eb566b32747b898f892ecf07f3d7c1b9
John_Ferrel <jf1223457796@gmail.com>
2021-12-17 17:02:01 +0800

    first commit

---
+++ Resource.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+RESOURCE_DIR = os.path.abspath(r'D:\大三上\人工智能\Final-PJ\pbrain-pyrandom-master\Gomoku-XYH19\final_project\code')
+'''
+if getattr(sys, 'frozen', False): #是否Bundle Resource
+    RESOURCE_DIR = sys._MEIPASS+'/resources'
+'''
